bef2c428dd32ccdc27b04dab2babaf749ed51abf
Edward Smale <essmale2005@gmail.com>
2022-12-01 22:19:03 +0000

    Update site design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,37 @@
 <script lang="ts">
     import "virtual:windi.css";
+    import { onDestroy, onMount } from "svelte";
 
     import { page } from "$app/stores";
     import DownArrow from "../icons/DownArrow.svelte";
 
-    $: console.log($page);
+    function onZoneUpdate(ev: HashChangeEvent) {
+        console.log(ev);
+    }
+
+    let scrollTop = 0;
+    function onScroll(ev: Event) {
+        scrollTop = document.documentElement.scrollTop;
+    }
+
+    onMount(() => {
+        if (typeof window !== "undefined") window.onhashchange = onZoneUpdate;
+        if (typeof document !== "undefined") document.addEventListener("scroll", onScroll);
+        if (typeof document !== "undefined") scrollTop = document.documentElement.scrollTop;
+    });
+
+    onDestroy(() => {
+        if (typeof window !== "undefined") window.onhashchange = null;
+        if (typeof document !== "undefined") document.removeEventListener("scroll", onScroll);
+    });
+
+    let windowInnerHeight: number;
 </script>
 
-<div class="w-full h-screen flex flex-col font-sans bg-zinc-900 text-zinc-300">
-    <div class="my-32 mx-16 flex flex-col gap-6 flex-1">
+<svelte:window bind:innerHeight={windowInnerHeight}></svelte:window>
+
+<div class="font-sans bg-zinc-900 text-zinc-300">
+    <div class="my-42 mx-16 flex flex-col gap-6 flex-1 fixed">
         <div class="flex items-end gap-2">
             <span class="text-7xl">Jade</span><span class="text-zinc-700">(she/her)</span>
         </div>
@@ -18,14 +41,25 @@
             <span class="text-2xl text-zinc-700">Game Developer</span>
             <span class="text-2xl text-zinc-800">Graphic Designer</span>
         </div>
-    </div>
-    <div class="flex justify-center flex-col items-center gap-8 m-8 mb-24">
-        <div class="flex flex-col items-center sm:flex-row sm:items-start">
-            <a href="/" class="text-2xl sm:border-r-1 sm:border-r-zinc-500 px-16" class:text-zinc-500={$page.route.id !== "/"}>Personal</a>
-            <a href="/college" class="text-2xl sm:border-l-1 sm:border-transparent px-16" class:text-zinc-500={$page.route.id !== "/college"}>College</a>
+        <div class="text-zinc-500">
+            jade@midlight.studio
         </div>
-        <span class="text-2xl text-zinc-500"><DownArrow size={32}/></span>
     </div>
 </div>
-
-<slot/>
+<div class="h-screen bg-zinc-900"></div>
+<div
+    class="sticky top-0 w-full flex justify-center items-center -mt-48 text-zinc-300 border-zinc-700" 
+    class:border-b-1={scrollTop > windowInnerHeight - 128}
+    class:bg-zinc-900={scrollTop > windowInnerHeight - 128}
+>
+    <div class="flex flex-col justify-center items-center gap-8 p-8">
+        <div class="flex flex-col items-center sm:flex-row sm:items-start">
+            <a href="/" class="text-2xl sm:border-r-1 sm:border-r-zinc-700 px-16 py-2" class:text-zinc-500={$page.route.id !== "/"}>Personal</a>
+            <a href="/college" class="text-2xl sm:border-l-1 sm:border-transparent px-16 py-2" class:text-zinc-500={$page.route.id !== "/college"}>College</a>
+        </div>
+        <span class="text-2xl text-zinc-500" style="opacity:{1 - (scrollTop / 200)}" class:hidden={scrollTop > 200}><DownArrow size={32}/></span>
+    </div>
+</div>
+<div class="bg-zinc-900 text-zinc-300">
+    <slot/>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,132 @@
-Personal
+<script lang="ts">
+    import { onDestroy, onMount } from "svelte";
+    import { goto } from "$app/navigation";
+    import { browser } from "$app/environment";
+
+    let projectSectionElements: HTMLDivElement[] = [];
+    let windowInnerHeight: number;
+
+    function reverseArray<T>(array: T[]) {
+        let reversedArray: T[] = [];
+        for (let i = array.length - 1; i >= 0; i--) {
+            reversedArray.push(array[i]);
+        }
+        return reversedArray;
+    }
+
+    let lastSectionInView: HTMLDivElement|undefined;
+    function onScroll() {
+        lastSectionInView = reverseArray(projectSectionElements).find(section => {
+            return section.getBoundingClientRect().top < windowInnerHeight;
+        });
+    }
+
+    onMount(() => {
+        if (typeof document !== "undefined") document.addEventListener("scroll", onScroll);
+        onScroll();
+    });
+
+    onDestroy(() => {
+        if (typeof document !== "undefined") document.removeEventListener("scroll", onScroll);
+    });
+
+    $: if (browser) goto(lastSectionInView ? "#" + lastSectionInView.id : "#", { replaceState: true, noScroll: true });
+</script>
+
+<svelte:window bind:innerHeight={windowInnerHeight}></svelte:window>
+
+<div class="w-full flex flex-col items-center pt-32">
+    <div class="flex flex-col w-[58rem]" id="edith" bind:this={projectSectionElements[0]}>
+        <span class="text-4xl">Edith</span>
+        <p>
+            Is this a dagger which I see before me,<br>
+            The handle toward my hand? Come, let me clutch thee.<br>
+            I have thee not, and yet I see thee still.<br>
+            Art thou not, fatal vision, sensible<br>
+            To feeling as to sight? or art thou but<br>
+            A dagger of the mind, a false creation,<br>
+            Proceeding from the heat-oppressed brain?<br>
+            I see thee yet, in form as palpable<br>
+            As this which now I draw.<br>
+            Thou marshall'st me the way that I was going;<br>
+            And such an instrument I was to use.<br>
+            Mine eyes are made the fools o' the other senses,<br>
+            Or else worth all the rest; I see thee still,<br>
+            And on thy blade and dudgeon gouts of blood,<br>
+            Which was not so before. There's no such thing:<br>
+            It is the bloody business which informs<br>
+            Thus to mine eyes. Now o'er the one halfworld<br>
+            Nature seems dead, and wicked dreams abuse<br>
+            The curtain'd sleep; witchcraft celebrates<br>
+            Pale Hecate's offerings, and wither'd murder,<br>
+            Alarum'd by his sentinel, the wolf,<br>
+            Whose howl's his watch, thus with his stealthy pace.<br>
+            With Tarquin's ravishing strides, towards his design<br>
+            Moves like a ghost. Thou sure and firm-set earth,<br>
+            Hear not my steps, which way they walk, for fear<br>
+            Thy very stones prate of my whereabout,<br>
+            And take the present horror from the time,<br>
+            Which now suits with it. Whiles I threat, he lives:<br>
+            Words to the heat of deeds too cold breath gives.
+        </p>
+    </div>
+    <div class="flex flex-col w-[58rem] mt-14" id="miss-you-on-the-train" bind:this={projectSectionElements[1]}>
+        <span class="text-4xl">I miss you on the train</span>
+        <p>
+            When I consider everything that grows<br>
+            Holds in perfection but a little moment,<br>
+            That this huge stage presenteth nought but shows<br>
+            Whereon the stars in secret influence comment;<br>
+            When I perceive that men as plants increase,<br>
+            Cheered and check'd even by the selfsame sky,<br>
+            Vaunt in their youthful sap, at height decrease,<br>
+            And wear their brave state out of memory;<br>
+            Then the conceit of this inconstant stay<br>
+            Sets you most rich in youth before my sight,<br>
+            Where wasteful Time debateth with Decay<br>
+            To change your day of youth to sullied night;<br>
+            And all in war with Time for love of you,<br>
+            As he takes from you, I engraft you new.
+        </p>
+    </div>
+    <div class="flex flex-col w-[58rem] mt-14" id="chicago" bind:this={projectSectionElements[2]}>
+        <span class="text-4xl">Chicago</span>
+        <p>
+            To be, or not to be: that is the question:<br>
+            Whether 'tis nobler in the mind to suffer<br>
+            The slings and arrows of outrageous fortune,<br>
+            Or to take arms against a sea of troubles,<br>
+            And by opposing end them? To die: to sleep;<br>
+            No more; and by a sleep to say we end<br>
+            The heart-ache and the thousand natural shocks<br>
+            That flesh is heir to, 'tis a consummation<br>
+            Devoutly to be wish'd. To die, to sleep;<br>
+            To sleep: perchance to dream: ay, there's the rub;<br>
+            For in that sleep of death what dreams may come<br>
+            When we have shuffled off this mortal coil,<br>
+            Must give us pause: there's the respect<br>
+            That makes calamity of so long life;<br>
+            For who would bear the whips and scorns of time,<br>
+            The oppressor's wrong, the proud man's contumely,<br>
+            The pangs of despised love, the law's delay,<br>
+            The insolence of office and the spurns<br>
+            That patient merit of the unworthy takes,<br>
+            When he himself might his quietus make<br>
+            With a bare bodkin? who would fardels bear,<br>
+            To grunt and sweat under a weary life,<br>
+            But that the dread of something after death,<br>
+            The undiscover'd country from whose bourn<br>
+            No traveller returns, puzzles the will<br>
+            And makes us rather bear those ills we have<br>
+            Than fly to others that we know not of?<br>
+            Thus conscience does make cowards of us all;<br>
+            And thus the native hue of resolution<br>
+            Is sicklied o'er with the pale cast of thought,<br>
+            And enterprises of great pith and moment<br>
+            With this regard their currents turn awry,<br>
+            And lose the name of action.-Soft you now!<br>
+            The fair Ophelia! Nymph, in thy orisons<br>
+            Be all my sins remember'd.'
+        </p>
+    </div>
+</div>
