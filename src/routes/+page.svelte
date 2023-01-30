ecfc18d25ff5c10ac77191dc043441e0d57ecc8d
Jade <110834742+Jadedot@users.noreply.github.com>
2023-01-30 02:42:29 +0000

    Added description text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -51,48 +51,46 @@
         <div class="absolute -top-32" id="edith" bind:this={projectSectionElements[0]}></div>
         <div class="flex items-center flex-col xl:items-start xl:flex-row gap-16">
             <div class="mx-8 sm:mx-0 sm:w-[30rem]">
-                <span class="text-4xl">Edith</span>
+                <span class="text-4xl">Edith - Concept Art Examples</span>
                 <p>
-                    Edithy Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec laoreet dui. Sed posuere ex vel augue ornare, non euismod ipsum imperdiet. Maecenas quis felis tempor, fringilla elit sed, eleifend mi. Vivamus quis efficitur nulla. Donec ac molestie lorem, imperdiet interdum ipsum. Nulla vestibulum arcu ut accumsan mattis. Phasellus tristique risus quis augue convallis porttitor. Fusce consectetur non libero et semper. Sed sagittis consectetur orci sed finibus. Suspendisse a interdum risus. Sed aliquam tellus vitae interdum rutrum. Aenean sodales arcu metus, vitae dictum ipsum bibendum vitae. Nulla vel velit laoreet, ultrices justo at, suscipit est. Aliquam volutpat semper lacinia.
-
-                    Etiam mattis leo lacus. Aliquam eget felis ac felis dapibus pharetra. Suspendisse potenti. Cras ultrices diam ut ante convallis placerat sit amet ut tortor. Nulla hendrerit leo eu lectus aliquet venenatis. Phasellus nulla tellus, sagittis vel lobortis eu, egestas et elit. Curabitur pulvinar nisl dui, a interdum lorem placerat sed. Vestibulum sed tincidunt sem. Sed sem arcu, pretium at rutrum sit amet, finibus sit amet lorem. Etiam blandit lobortis nisl. Phasellus purus odio, rhoncus quis nisi a, vulputate egestas est. Proin ornare purus ac accumsan ultricies. Phasellus et cursus risus, sed posuere turpis. 
+                    To the right are just a few samples of concept art drawn for characters in a project going by the name "Edith". Edith is a passion project started by myself and a very close friend of mine and is currently one year into development, with it's demo a very early work in progress. Inspired by OMOCAT's "OMORI", Edith is a top-down RPG loveletter to the aformentioned title, with my primary role in its creation being art direction for both final pieces along with concept artwork and designs (with a secondary but still large focus on story and character develoments). The images to the right are clickable, and should display a small and relevant caption. The date at which each piece of concept art was made is written in red pen to give an idea as to which are early works wnd which are more recent.
                 </p>
             </div>
             <div class="2xl:w-[42rem] sm:w-[28rem] w-[20rem]">
                 <Grid cols={5} rows={12}>
                     <Cell col={1} row={1} colSpan={4} rowSpan={3}>
                         <ViewableImage alt="OC character Cluo leaning" src="cluolean.png">
-                            Cluo is my baby, as you can see from this image
+                            One of the more dynamic poses I referenced for Edith's deuteragonist, 'Cluo', including two slighly different designs, concepted to show small differences in her appearence that might be made depending on what difficulty the game is set at.
                         </ViewableImage>
                     </Cell>
                     <Cell col={5} row={1} colSpan={1} rowSpan={3}>
                         <div class="mt-auto">
                             <ViewableImage alt="OC character Macula in both male and female versions" src="macula.png">
-                                Macula is a little itty bitty spider who climbed up the water spout
+                                Some "finalised" artwork of Edith's protagonist, 'Macula', when considering letting the player pick their gender. It was eventually decided, however, that the left design would be used.
                             </ViewableImage>
                         </div>
                     </Cell>
                     <Cell col={3} row={4} colSpan={3} rowSpan={5}>
                         <ViewableImage alt="Draft art showing various emotions for OC character Cluo" src="cluoemotions.png">
-                            Bipolar Cluo designs
+                            The first ever concept drawings done for Edith, an emotion sheet showing a gameplay mechanic dubbed "patience". This was the first of many emotion charts I ended up drawing for Edith.
                         </ViewableImage>
                     </Cell>
                     <Cell col={1} row={4} colSpan={2} rowSpan={6}>
                         <div class="flex flex-col gap-2">
                             <ViewableImage alt="OC character Cluo in various poses and stages" src="cluopipleine.png">
-                                I wanna know, have you ever seen the rain
+                                A page showing some progression of how I choose to draw Edith's concept art, along with some processes of how I take an older design and modernise them with my updated drawing style.
                             </ViewableImage>
                             <ViewableImage alt="Draft art for video game end scene" src="gameover.png">
-                                A dreaded sunny day
+                                Unfinished concept art for the pose taken by Cluo during a proposed "Game Over" screen. The intention would be that her hand be hovering over an analgoue clock, though I'm still yet to finish this.
                             </ViewableImage>
                             <ViewableImage alt="OC character Iris in various styles" src="iris.png">
-                                Some description
+                                The difference in only a few months work in designing one of Edith's bosses, "Iris". After a new idea for her design along with some new skills, this is probably one of my best modernisations of older artwork.
                             </ViewableImage>
                         </div>
                     </Cell>
                     <Cell col={3} row={7} colSpan={3} rowSpan={3}>
                         <ViewableImage alt="OC character camina" src="camina.png">
-                            Camina progressing through the human evolutionary process
+                            Despite the artwork being quite dated at this point in time, I'd still like to show this one as it's evidence of how I used to (and largely still do) refine my artwork, by drawing over it a lot until I'm happy with it. It's an ineffecient process that I'm trying to stop, but for now it helps me get my ideas across.
                         </ViewableImage>
                     </Cell>
                 </Grid>
@@ -103,11 +101,9 @@
         <div class="absolute -top-32" id="animation" bind:this={projectSectionElements[1]}></div>
         <div class="flex items-center flex-col xl:items-start xl:flex-row gap-16">
             <div class="mx-8 sm:mx-0 sm:w-[30rem]">
-                <span class="text-4xl">Animation</span>
+                <span class="text-4xl">Edith - Sprite Animation Examples</span>
                 <p>
-                    Edith Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec laoreet dui. Sed posuere ex vel augue ornare, non euismod ipsum imperdiet. Maecenas quis felis tempor, fringilla elit sed, eleifend mi. Vivamus quis efficitur nulla. Donec ac molestie lorem, imperdiet interdum ipsum. Nulla vestibulum arcu ut accumsan mattis. Phasellus tristique risus quis augue convallis porttitor. Fusce consectetur non libero et semper. Sed sagittis consectetur orci sed finibus. Suspendisse a interdum risus. Sed aliquam tellus vitae interdum rutrum. Aenean sodales arcu metus, vitae dictum ipsum bibendum vitae. Nulla vel velit laoreet, ultrices justo at, suscipit est. Aliquam volutpat semper lacinia.
-
-                    Etiam mattis leo lacus. Aliquam eget felis ac felis dapibus pharetra. Suspendisse potenti. Cras ultrices diam ut ante convallis placerat sit amet ut tortor. Nulla hendrerit leo eu lectus aliquet venenatis. Phasellus nulla tellus, sagittis vel lobortis eu, egestas et elit. Curabitur pulvinar nisl dui, a interdum lorem placerat sed. Vestibulum sed tincidunt sem. Sed sem arcu, pretium at rutrum sit amet, finibus sit amet lorem. Etiam blandit lobortis nisl. Phasellus purus odio, rhoncus quis nisi a, vulputate egestas est. Proin ornare purus ac accumsan ultricies. Phasellus et cursus risus, sed posuere turpis. 
+                     To the right are some examples of the in-game spritework test animations done for the previously mentioned early unpolished demo currently being worked on for Edith, along with a screen recording containing some of them actually being used. These animations include: breathing, idling, attacking, jumping and crouching motions, as well as taking damage. While they don't look particuarly animated on their own, once given context with each other, they flow well into one another for the type of gameplay we hope Edith is going to have. Some older attempts at these aniamtions should be seen below the screen recording.
                 </p>
             </div>
             <div class="2xl:w-[42rem] sm:w-[28rem] w-[20rem]">
